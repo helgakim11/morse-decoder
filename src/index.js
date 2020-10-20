22bd2430e63182eb8a516252573499f742b8312f
Helga Maznikava <helga@helgas-air.lan>
2020-10-19 18:33:46 -0700

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morseEncodedChars = [];
+    for(let i = 0; i < expr.length; i += 10) {
+        let str = expr.slice(i, i + 10); //string
+        if (!str.includes("*")) {
+            str = Number(str).toString();
+            let morseCode = '';
+            for (let j = 0; j < str.length; j += 2) {
+                let code = str.slice(j, j + 2);
+                if(code === '10') {
+                    morseCode += '.';
+                } else if (code === '11') {
+                    morseCode += '-';
+                }
+            }
+            str = MORSE_TABLE[morseCode];
+        } else {
+            str = ' ';
+        }
+        morseEncodedChars.push(str);
+
+    }
+    return morseEncodedChars.join('');
 }
 
 module.exports = {
